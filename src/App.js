d59c2fb250b7610e4a08f19838b6c4d03aed9604
jhon11390 <meljhon2@gmail.com>
2020-04-01 08:44:14 -0500

    Se realiza el contructor y la funcion de incremento (changeNumber)

--- src/App.js
+++ src/App.js
@@ -2,13 +2,25 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+  constructor(){
+    super();
+
+    this.state = {
+      number: 0
+    }
+  }
   render() {
     return (
       <div>
-        <span className="value">0</span>
-        <button id="inc">Incrementa</button>
+        <span className="value">{this.state.number}</span>
+        <button id="inc" onClick={this.changeNumber.bind(this)}>Incrementa</button>
       </div>
     );
+  }
+  changeNumber(){
+    this.setState({
+      number: this.state.number += 1
+    })
   }
 }
 
